cd7a72a46ff5122eace9175a332a9fca072e787f
martinmejlop <martinmejiaslopez@gmail.com>
2024-03-06 20:31:14 +0100

    Modifico el nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav>
     <!-- Rellena las rutas que faltan -->
-    <a class="btn btn-info" href="{{ route('usuarios.index') }}" role="button">Inicio|</a>
-    <a class="btn btn-info" href="{{ route('usuarios.create') }}" role="button">Nuevos|</a>
-    <a class="btn btn-info" href="{{ route('usuarios.destacados') }}" role="button">Destacados</a>
+    <a class="btn btn-info" href="{{ route('chollos.index') }}" role="button">Inicio|</a>
+    <a class="btn btn-info" href="{{ route('chollos.create') }}" role="button">Nuevos|</a>
+    <a class="btn btn-info"  role="button">Destacados</a>
 </nav>
