872ecaef1103fcf667d1dfd91e7d2c42f9a8fc64
martinmejlop <martinmejiaslopez@gmail.com>
2024-03-06 18:59:47 +0100

    Creo el nav

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,6 @@
+<nav>
+    <!-- Rellena las rutas que faltan -->
+    <a class="btn btn-info" href="{{ route('usuarios.index') }}" role="button">Inicio|</a>
+    <a class="btn btn-info" href="{{ route('usuarios.create') }}" role="button">Nuevos|</a>
+    <a class="btn btn-info" href="{{ route('usuarios.destacados') }}" role="button">Destacados</a>
+</nav>
